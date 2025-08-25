aadd0fe3e3b06eb98efbac4367b35a8f0d8acd5e
Luke Thornton <lukethornton117@gmail.com>
2025-08-25 16:10:44 +0100

    layout: input + list shell

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule, NgFor } from '@angular/common';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonItem, IonInput, IonButton, IonList, IonLabel
+} from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+
+//task structure
+type Task = { id: number; title: string; done: boolean };
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent]
+  //ion imports
+  imports: [
+    CommonModule, FormsModule, NgFor,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonItem, IonInput, IonButton, IonList, IonLabel
+  ]
 })
 export class HomePage {
-  
+  //task list
+  tasks: Task[] = [];
+  newTask = '';
+
+
 }
